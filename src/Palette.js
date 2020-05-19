40694118015719f45889d2f36874ee7f059a3bc5
InsideTheMatrix <pspratham16@gmail.com>
2020-05-19 09:43:02 +0530

    Minor teaws

--- src/Palette.js
+++ src/Palette.js
@@ -4,8 +4,12 @@ import './Palette.css';
 class Palette extends Component {
     render(){
         return(
-            <div>
-                <h1>Palette</h1>
+            <div className="Palette">
+            {/* NavBat goes here */}
+                <div className="Palette-colors">
+                    {/* bunch of color boxes */}
+                </div>
+                {/* Footer eventually */}
             </div>
         );
 
