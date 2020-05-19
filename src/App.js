ca89b40cf5ad188b7ce7724b8930beaee76dfead
InsideTheMatrix <pspratham16@gmail.com>
2020-05-19 10:34:17 +0530

    add color box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
+import seedColors from './seedColors';
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <Palette />
+        <Palette {...seedColors[5]}/>
       </div>
     );
   }
