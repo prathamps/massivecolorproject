f5ecc152bf1a9cf2950f1b133a651e32cb76592c
InsideTheMatrix <pspratham16@gmail.com>
2020-05-19 09:37:07 +0530

    Initial Commit, Palette Added

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Palette.css';
+
+class Palette extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Palette</h1>
+            </div>
+        );
+
+    }
+}
+
+export default Palette;
