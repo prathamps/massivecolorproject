6da37cd56dedf7bbec95a3192664e45b38a28a5a
InsideTheMatrix <pspratham16@gmail.com>
2020-05-23 13:04:24 +0530

    Display Colrs in single color palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import './App.css';
@@ -16,6 +17,17 @@ class App extends Component{
   render(){   
     return(
       <Switch>
+
+        <Route 
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={routeProps =>
+          <SingleColorPalette 
+          colorId={routeProps.match.params.colorId}
+            palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+          />}
+        />
+       
         <Route 
           exact 
           path='/'
@@ -28,12 +40,6 @@ class App extends Component{
           render={routeProps => <Palette palette={ generatePalette(this.findPalette(routeProps.match.params.id))} /> }
           />
 
-        <Route 
-          exact
-          path='/palette/:paletteId/:colorId'
-          render={()=><h1>Single Color Page!</h1>}
-        />
-     
       </Switch>
 
 
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -14,7 +14,7 @@ class ColorBox extends Component {
         })
     }
     render(){
-        const {name,background} = this.props;
+        const {name, background, id, paletteId, showLink} = this.props;
         const {copied} = this.state;
         return(
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -30,9 +30,11 @@ class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <Link to="/" onClick={e => e.stopPropagation()}>
-                        <span className="see-more">MORE</span>
-                    </Link>   
+                    {showLink &&
+                        <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
+                            <span className="see-more">MORE</span>
+                        </Link>   
+                    }
                 </div>
             </CopyToClipboard>
         );
