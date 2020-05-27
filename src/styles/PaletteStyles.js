00e260f2cb6edc45afe7dc65f4c68625d765f4b8
InsideTheMatrix <pspratham16@gmail.com>
2020-05-27 22:48:38 +0530

    Make Color Box Responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
    
     Palette: {
@@ -37,8 +39,18 @@ export default {
             cursor: "pointer",
             border: "none",
             textDecoration: "none"
-        }
-
-       
+        },
+        [sizes.down("lg")]: {
+            width: "25%",
+            height: "33.3333%"
+        },
+        [sizes.down("md")]: {
+            width: "50%",
+            height: "20%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: "10%"
+        },  
     }
 };
