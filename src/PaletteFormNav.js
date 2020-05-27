55797d01bdb50ad62ac22f99697525a12695eb53
InsideTheMatrix <pspratham16@gmail.com>
2020-05-27 12:30:55 +0530

    Tweak styles of form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import Button from '@material-ui/core/Button';
 import styles from './styles/PaletteFormNavStyles';
 
@@ -46,12 +46,14 @@ class PaletteFormNav extends Component {
                 >
                 <Toolbar disableGutters={!open}>
                     <IconButton
-                    color="inherit"
-                    aria-label="Open drawer"
-                    onClick={handleDrawerOpen}
-                    className={classNames(classes.menuButton, open && classes.hide)}
+                        color="inherit"
+                        aria-label="Open drawer"
+                        onClick={handleDrawerOpen}
+                        className={classNames(classes.menuButton, {
+                            [classes.hide]: open
+                        })}
                     >
-                    <MenuIcon />
+                        <AddToPhotosIcon />
                     </IconButton>
                     <Typography variant="h6" color="inherit" noWrap>
                     Create Your Palette
