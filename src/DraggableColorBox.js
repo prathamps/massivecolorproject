fa9350a68ba8c17580a3f1085d110d524a2edf21
InsideTheMatrix <pspratham16@gmail.com>
2020-05-27 12:14:38 +0530

    Move Styles out and add constants file

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,38 +2,7 @@ import React from 'react';
 import { SortableElement } from "react-sortable-hoc";
 import DeleteIcon from '@material-ui/icons/Delete';
 import { withStyles } from '@material-ui/styles';
-
-const styles = {
-    root: {
-        width:"20%",
-        height: "25%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-6px",
-        "&:hover svg": {
-            color: "white",
-            transform: "scale(1.3)"
-        }
-    },
-    boxContent: {
-        position: "absolute",
-        width: "100%",
-        left: "0px",
-        bottom: "0px",
-        padding: "10px",
-        color: "rgba(0, 0, 0, 0.5)",
-        letterSpacing: "1px",
-        textTransform: "uppercase",
-        fontSize: "12px",
-        display: "flex",
-        justifyContent: "space-between"
-    },
-    deleteIcon: {
-        transition: "all 0.3s ease-in-out"
-    }
-};
+import styles from './styles/DraggableColorBoxStyles';
 
 const DraggableColorBox = SortableElement((props) => {
         const { classes, handleClick, name, color } = props;
