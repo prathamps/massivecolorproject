c28522d188e814f8888f7b46e899425e1f3a118d
InsideTheMatrix <pspratham16@gmail.com>
2020-05-28 13:04:00 +0530

    Fix navbar slider sides and add classNames

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styles from './styles/ColorBoxStyles';
 import { withStyles } from '@material-ui/styles';
-
+import classNames from 'classnames'
 
 
 class ColorBox extends Component {
@@ -29,8 +29,8 @@ class ColorBox extends Component {
         return(
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={classes.ColorBox}>
-                    <div style={{ background }} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
-                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
+                    <div style={{ background }} className={classNames(classes.copyOverlay, {[classes.showOverlay]: copied})} />
+                    <div className={classNames(classes.copyMessage, {[classes.showMessage]: copied})}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
